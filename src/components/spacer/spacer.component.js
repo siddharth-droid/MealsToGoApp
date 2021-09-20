ce5cd36450240aa69785cde021a5bf6d91aeb88e
Siddharth Negi <sidharthnegi43@gmail.com>
2021-09-20 13:06:32 +0530

    Added styles to seperate styles file

--- src/components/spacer/spacer.component.js
+++ src/components/spacer/spacer.component.js
@@ -1,26 +1,38 @@
-import styled from "styled-components/native";
+import React from "react";
+import styled, { useTheme } from "styled-components/native";
 
 const sizeVariant = {
-  small:1,
-  medium:2,
-  large:3,
-}
+  small: 1,
+  medium: 2,
+  large: 3,
+};
 
 const positionVariant = {
-    top:'marginTop',
-    left:'marginLeft',
-    right:'marginRight',
-    bottom:'marginBottom',
-}
+  top: "marginTop",
+  left: "marginLeft",
+  right: "marginRight",
+  bottom: "marginBottom",
+};
 
-const getVariant = (position,size) => `${positionVariant[position]}:${sizeVariant[size]}`;
+const getVariant = (position, size, theme) => {
+  const sizeIndex = sizeVariant[size];
+  const property = positionVariant[position];
+  const value = theme.space[sizeIndex];
 
-export const Spacer = styled.View`
-${(position,size) => getVariant(position,size)}
+  return `${property}:${value}`;
+};
+
+const SpacerView = styled.View`
+  ${({ variant }) => variant};
 `;
 
+export const Spacer = ({ position, size, children }) => {
+  const theme = useTheme();
+  const variant = getVariant(position, size, theme);
+  return <SpacerView variant={variant}>{children}</SpacerView>;
+};
 
 Spacer.defaultprops = {
-  position = 'top',
-  size = 'small'
-}
+  position: "top",
+  size: "small",
+};
