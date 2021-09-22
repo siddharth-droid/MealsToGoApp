2a01b0e338a11f3482905a5d7759d9a6570ddfec
Siddharth Negi <sidharthnegi43@gmail.com>
2021-09-22 23:29:37 +0530

    Fixed list issues

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -14,6 +14,10 @@ const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const RestaurantList = styled(FlatList)`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
 export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
