33e535253d909f25b21ee348c0571cba2a6356e2
Siddharth Negi <sidharthnegi43@gmail.com>
2021-10-10 21:19:38 +0530

    Added background image in account screen

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,8 +1,6 @@
 import React from "react";
+import { AccountBackground } from "../components/account.styles";
 
-
-const AccountScreen = () =>{
-    return (
-
-    );
-}
+export const AccountScreen = () => {
+  return <AccountBackground />;
+};
