c2d469f6510bdc27926ff6edfc01396ea8f5b8ad
Siddharth Negi <sidharthnegi43@gmail.com>
2021-10-14 23:46:01 +0530

    Created register screen

--- src/services/authentication/authentication.context.js
+++ src/services/authentication/authentication.context.js
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from "react";
 import { loginRequest } from "./authentication.service";
+import * as firebase from "firebase";
 
 export const AuthenticationContext = createContext();
 
@@ -21,11 +22,27 @@ export const AuthenticationContextProvider = ({ children }) => {
       });
   };
 
-  const onRegister
+  const onRegister = (email, password, repeatedPassword) => {
+    if (password !== repeatedPassword) {
+      setError("Error: Passwords do not match!");
+      return;
+    }
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then((u) => {
+        setUser(u);
+        setIsLoading(false);
+      })
+      .catch((e) => {
+        setIsLoading(false);
+        setError(e.toString());
+      });
+  };
 
   return (
     <AuthenticationContext.Provider
-      value={{ isAuthenticated: !!user, isLoading, error, onLogin }}
+      value={{ isAuthenticated: !!user, isLoading, error, onLogin, onRegister }}
     >
       {children}
     </AuthenticationContext.Provider>
