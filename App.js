e5e1a7cd234eab8c58e5abf978820741bfc323b8
Siddharth Negi <sidharthnegi43@gmail.com>
2021-10-16 20:56:35 +0530

    Added watermelon animation

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
 import * as firebase from "firebase";
--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import LottieView from "lottie-react-native";
+
 import { Spacer } from "../../../components/spacer/spacer.component";
 
 import {
@@ -8,12 +10,23 @@ import {
   AccountCover,
   AuthButton,
   Title,
+  AnimationWrapper,
 } from "../components/account.styles";
+import styled from "styled-components/native";
 
 export const AccountScreen = ({ navigation }) => {
   return (
     <AccountBackground>
       <AccountCover />
+      <AnimationWrapper>
+        <LottieView
+          key="animation"
+          autoPlay
+          loop
+          resizeMode="cover"
+          source={require("../../../../assets/watermelon.json")}
+        />
+      </AnimationWrapper>
       <Title>Meals To Go</Title>
       <AccountContainer>
         <AuthButton
